fc074125afde3e2f4ed22e8f9c051ef688ae8ad0
amir141034 <64488465+amir141034@users.noreply.github.com>
2026-02-02 21:02:59 +0800

    color mode button

--- app/app.vue
+++ app/app.vue
@@ -6,6 +6,10 @@
     <main class="w-full overflow-x-hidden">
       <NuxtPage />
     </main>
+
+    <div class="fixed top-4 right-4 z-50">
+      <UColorModeButton />
+    </div>
     
     <footer class="bottom-0 w-full mt-3 py-6 max-md:hidden">
       <p class="text-center px-4">Tip: Add missing keywords naturally</p>
